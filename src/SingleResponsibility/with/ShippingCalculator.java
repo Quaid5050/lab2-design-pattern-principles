16ba52e55ea2ed18eb43370da41fde72cbd36352
Quaid Ahmed <inoxent5050@gmail.com>
2023-09-15 23:51:24 +0500

    Single-responsibility principle

---
+++ src/SingleResponsibility/with/ShippingCalculator.java
@@ -0,0 +1,7 @@
+package SingleResponsibility.with;
+
+public class ShippingCalculator {
+    public int calculateShippingCost(int price, double kilometer) {
+        return (int) (price * kilometer);
+    }
+}
