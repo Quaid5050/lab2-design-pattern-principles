8761796cdfd67344a54beb6f02a8a986cfd0cbc2
Quaid Ahmed <inoxent5050@gmail.com>
2023-10-26 11:04:49 +0500

    implement template method

---
+++ src/DesignPatterns/TemplateMethod/NetworkType.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DesignPatterns.TemplateMethod;
+
+/**
+ *
+ * @author Zain
+ */
+public enum NetworkType {
+    None,      // Represents no specific network
+    Facebook,
+    Twitter,
+    LinkedIn
+}
+
+
+
+
